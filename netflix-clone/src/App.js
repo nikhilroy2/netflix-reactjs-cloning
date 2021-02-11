767c5d78c28d20af07e544ddfd37647ff2c827df
FENR1 USER <fenr1user@biadsconsulting.org>
2021-02-11 21:46:54 +0600

    reactjs netflix work 1 hours

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
 function App() {
   return (
     <div className="App">
-      
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
     </div>
   );
 }
 
 export default App;
+ 
