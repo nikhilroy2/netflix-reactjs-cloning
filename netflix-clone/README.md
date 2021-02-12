00a287892f3a62ea7214a385a04bc0ed9999f063
FENR1 USER <fenr1user@biadsconsulting.org>
2021-02-12 13:57:34 +0600

    duration 2.25hours finish

--- netflix-clone/README.md
+++ netflix-clone/README.md
@@ -1,2 +1,5 @@
 Thursday 11 february 2021:
 Duration: 1.10hours finish
+Friday:
+Duration: 1.30hours finish
+Duration: 2.25hours finish
--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 function App() {
   return (
-    <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}></Row>
+    <div className="app">
+      {/* Nav */}
+      {/* Banner */}
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      ></Row>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries Movies" fetchUrl={requests.fetchTrending}></Row>
     </div>
   );
 }
 
 export default App;
- 
